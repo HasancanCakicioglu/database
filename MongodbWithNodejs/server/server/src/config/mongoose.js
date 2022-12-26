5593159bfe588c8148a56dd25742d33f51dac70d
IstakozNecmi <96948396+IstakozNecmi@users.noreply.github.com>
2022-12-26 11:42:31 +0300

    Update mongoose.js

--- MongodbWithNodejs/server/server/src/config/mongoose.js
+++ MongodbWithNodejs/server/server/src/config/mongoose.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
         //run().catch(console.dir);
-        mongoose.connect("mongodb+srv://testUser:123456test@cluster0.vk6odsj.mongodb.net/test?retryWrites=true&w=majority" ,{ useNewUrlParser: true, useUnifiedTopology: true })
+        mongoose.connect( ,{ useNewUrlParser: true, useUnifiedTopology: true })
     } catch (error) {
         console.log(error)
      process.exit(1)
